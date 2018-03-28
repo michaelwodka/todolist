dcffb6dfa6c7698d204d4b90f7fd43ccc9d2c8e5
mwodka <michaelwodka12@gmail.com>
2018-03-28 16:29:47 -0700

    major update

---
+++ script.js
@@ -0,0 +1,19 @@
+var row = [
+		'<div class = "row">',
+			'<div class = "col-xs-12 col-lg-4 col-lg-offset-4">',
+				'<div class ="box">&nbsp;</div>',
+			'</div>',
+		'</div>'
+];
+
+$(function(){
+	var windowh = $(window).height()
+	var boxh = $(".box").height()
+    var iter= Math.round((3/4)*(windowh/boxh))
+    $.each(new Array(iter),function(){ 
+        $("#adder").after(row);
+    });
+
+    $(".box:odd").css("background-color", "rgb(224,224,224)");
+});
+
