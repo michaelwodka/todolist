ed1fd902dbf57c3d0c19b1cd7d73f08ce9f8f02d
mwodka <michaelwodka12@gmail.com>
2018-03-30 15:55:12 -0700

    another update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function setup(){
+$(document).ready(function toDoListSetup(){
 	var row = (
 			'<div class = "row">' +
 				'<div class = "col-xs-12 col-lg-4 col-lg-offset-4">' +
@@ -16,47 +16,58 @@ $(document).ready(function setup(){
     });
 
     $(".box:odd").css("background-color", "rgb(202,222,245)");
+    $('#adder').select();
 });
 
-function clicker(){
+function addToDoItem(){
 	var todo = $('#adder').val();
 	var currentcolor =  $(this).css("background-color")
 	var nextcolor = $(this).parent().parent().next().find('.box').css("background-color")
 
-	$(this).parent().parent().next().replaceWith(
-		'<div class = "row">' +
-			'<div class = "col-xs-12 col-lg-4 col-lg-offset-4">' +
-				'<div class = "col-xs-1 nopadding borderleft" id = "clicker">' +
-					'<div id = "plus">&#43;</div>' +
+	if ($('#adder').val()){
+
+		$(this).parent().parent().next().replaceWith(
+			'<div class = "row">' +
+				'<div class = "col-xs-12 col-lg-4 col-lg-offset-4">' +
+					'<div class = "col-xs-1 nopadding borderleft" id = "clicker">' +
+						'<div id = "plus">&#43;</div>' +
+					'</div>' +
+					'<div class = "col-xs-11 nopadding">' +
+						'<input type="text" id = "adder" placeholder = "New to-do...">' +
+					'</div>' +
 				'</div>' +
-				'<div class = "col-xs-11 nopadding">' +
-					'<input type="text" id = "adder" placeholder = "New to-do...">' +
-				'</div>' +
-			'</div>' +
-		'</div>'
-	);
+			'</div>'
+		);
 
-	$('#clicker').replaceWith(
-		'<div class = "col-xs-1 nopadding borderleft">' +
-			'<input id = "checker" type="checkbox">' +
-		'</div>'	
-	);
+		$('#clicker').replaceWith(
+			'<div class = "col-xs-1 nopadding borderleft">' +
+				'<input class = "checker" type="checkbox">' +
+			'</div>'	
+		);
 
-	$('#adder').replaceWith('<div id = "todotext">' + todo + '</div>');
+		$('#adder').replaceWith('<div class = "todotext">' + todo + '</div>');
 
-	$('#clicker').css('background-color', nextcolor)
-	$('#adder').css('background-color', nextcolor)
+		$('#clicker').css('background-color', nextcolor)
+		$('#adder').css('background-color', nextcolor)
+		$('#adder').select();
 
-	console.log($(this).parent().prev().attr('id'))
+		$('.todotext:last').css('background-color', currentcolor)
+		$('.checker:last').parent().css('background-color', currentcolor)
 
-	$('#todotext').css('background-color', currentcolor)
-	$('#checker').parent().css('background-color', currentcolor)
+		setAddToDoItemEvent();
+	} else{
 
-	setter();
+		$('#adder').attr("placeholder", "To-do cannot be left blank. Try again.")
+	}
 };
 
-function setter(){
-	setTimeout(function(){$("#clicker").on("click", clicker)}, 100);
+function setAddToDoItemEvent(){
+	setTimeout(function(){$("#clicker").on("click", addToDoItem)}, 100);
+	setTimeout(function(){$("#adder").keypress(function(event){
+		if (event.which === 13){
+			$("#clicker").click();
+		}
+	})}, 100);
 };
 
-setter();
+setAddToDoItemEvent();
