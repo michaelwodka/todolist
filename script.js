bca388afd4f37f60f7036340c0f51cb283d8ac5a
mwodka <michaelwodka12@gmail.com>
2018-03-29 21:17:22 -0700

    another update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(function setup(){
+$(document).ready(function setup(){
 	var row = (
 			'<div class = "row">' +
 				'<div class = "col-xs-12 col-lg-4 col-lg-offset-4">' +
@@ -15,31 +15,48 @@ $(function setup(){
         $("#adderdiv").after(row);
     });
 
-    $(".box:odd").css("background-color", "rgb(224,224,224)");
-
-    $('#clicker').on("click", function(){
-    	var todo = $('#adder').val();
-    	$('#plus').replaceWith(
-    		'<input id = "checker" type="checkbox">'
-    	);
-    	$('#adder').replaceWith(
-    		'<div id = "todotext">' + todo + '</div>'
-    	);
-
-    	$(this).parent().parent().next().replaceWith(
-    		'<div class = "row" id = "adderdiv">' +
-				'<div class = "col-xs-12 col-lg-4 col-lg-offset-4">' +
-					'<div class = "col-xs-1 nopadding borderleft" id = "clicker">' +
-						'<div id = "plus">&#43;</div>' +
-					'</div>' +
-					'<div class = "col-xs-11 nopadding">' +
-						'<input type="text" id = "adder" placeholder = "New to-do...">' +
-					'</div>' +
-				'</div>' +
-			'</div>'
-    	);
-    	$(".box:odd").css("background-color", "none");
-    	$(".box:even").css("background-color", "rgb(224,224,224)");
-	});
+    $(".box:odd").css("background-color", "rgb(202,222,245)");
 });
 
+function clicker(){
+	var todo = $('#adder').val();
+	var currentcolor =  $(this).css("background-color")
+	var nextcolor = $(this).parent().parent().next().find('.box').css("background-color")
+
+	$(this).parent().parent().next().replaceWith(
+		'<div class = "row">' +
+			'<div class = "col-xs-12 col-lg-4 col-lg-offset-4">' +
+				'<div class = "col-xs-1 nopadding borderleft" id = "clicker">' +
+					'<div id = "plus">&#43;</div>' +
+				'</div>' +
+				'<div class = "col-xs-11 nopadding">' +
+					'<input type="text" id = "adder" placeholder = "New to-do...">' +
+				'</div>' +
+			'</div>' +
+		'</div>'
+	);
+
+	$('#clicker').replaceWith(
+		'<div class = "col-xs-1 nopadding borderleft">' +
+			'<input id = "checker" type="checkbox">' +
+		'</div>'	
+	);
+
+	$('#adder').replaceWith('<div id = "todotext">' + todo + '</div>');
+
+	$('#clicker').css('background-color', nextcolor)
+	$('#adder').css('background-color', nextcolor)
+
+	console.log($(this).parent().prev().attr('id'))
+
+	$('#todotext').css('background-color', currentcolor)
+	$('#checker').parent().css('background-color', currentcolor)
+
+	setter();
+};
+
+function setter(){
+	setTimeout(function(){$("#clicker").on("click", clicker)}, 100);
+};
+
+setter();
